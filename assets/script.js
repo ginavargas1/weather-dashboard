dd09788ee310eefa9b9e6a5fed2e39e911fba4e9
Gina Vargas <gina.vargas89@outlook.com>
2021-02-28 13:11:34 -0800

    added new id to html

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,32 @@
 // variables for search for content, history
 var searchFormEl = $('#search-input');
-var button = $("#search-button");
-var current = $("#today");
-var temperature = $("#weather");
+var searchButton = $("#search-button");
+var currentCity = $("#current-city");
+var currentHumidity = $("#humidity");
+var currentWindSpeed = $("#wind-speed");
+var uvIndex = $("#uv-index");
+var currentDate = $("#today");
+var currentTemp = $("#weather");
 
 function handleSearchFormSubmit() {
 
   // Weather API 
+  var apiKey = "9c6df7d93197a62075c56f857ce0bf62";
+
+  function displayWeather(event){
+    event.preventDefault();
+    if(searchFormEl.val().trim()!=="") {
+      city=searchCiy.val().trim();
+    }
+  }
+
   var city = searchFormEl.val()
   var urlRequest = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=9c6df7d93197a62075c56f857ce0bf62`                   
+
+  var urlRequest2 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=9c6df7d93197a62075c56f857ce0bf62`                   
+  console.log(urlRequest2)
+
+
 
   $.ajax({
     url: urlRequest,
@@ -16,24 +34,38 @@ function handleSearchFormSubmit() {
   }).then(function (response) {
     console.log('Ajax Reponse \n-------------');
     console.log(response);
-    current.text(new Date().getDate())
-    temperature.text(response.list[0].main.temp + " F")
+    currentDate.text(new Date().getDate("M/D/YYYY"))
+    currentTemp
+  .text(response.list[0].main.temp + " F")
 
     for(const data of response.list) {
       console.log(data);
       if(data.dt_txt.indexOf("12:00:00")!== -1) {
         console.log(data.dt_txt);
-         current.text(new Date().getDate())
-         temperature.text(data.main.temp + " F")
+         currentDate.text(new Date().getDate())
+         currentTemp
+        .text(data.main.temp + " F")
       }
        
+      var weatherIcon = $("<img>");
+        weatherIcon.attr(
+          "src",
+          
+          "http://openweathermap.org/img/wn/" + weather[0].ui-icon + ".png", 
+            
+          console.log(icon),
+        console.log(weatherIcon)
+        )
+          // + weather.weather[0].icon + ".png"
+        $("#currentDate-icon").empty();
+        $("#currentDate-icon").append(weatherIcon);
 
     }
 
-
+s
   });
 }
-button.on('click', () => {
+searchButton.on('click', () => {
   handleSearchFormSubmit()
 });
 
@@ -42,3 +74,4 @@ button.on('click', () => {
 // depending on the weather UV index color will need to change > use css, three different colors
 // Weather must include 5 day forcast 
 // 
+
